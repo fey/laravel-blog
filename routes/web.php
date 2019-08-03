3ae9bc384f7ee1c35b92e20e10f081ee4dc61cc5
feycot <feycot@gmail.com>
2019-08-03 20:46:03 +0500

    add about page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +12,7 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/about', function () {
+    return view('about');
 });
