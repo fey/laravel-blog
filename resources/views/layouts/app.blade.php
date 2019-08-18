2635e2f491cefcba4433ca5944db1fbf8177c6cd
feycot <feycot@gmail.com>
2019-08-18 17:16:24 +0500

    move links to center

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="csrf-param" content="_token" />
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Fey blog') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Fey blog</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -65,19 +65,6 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
 
             <div class="content">
                 <div class="title m-b-md">
@@ -85,8 +72,18 @@
                 </div>
 
                 <div class="links">
+                    @if (Route::has('login'))
+                            @auth
+                                {{-- <a href="{{ url('/home') }}">Home</a> --}}
+                                <a href="{{ route('article.create') }}">Create</a>
+                            @else
+                                <a href="{{ route('login') }}">Login</a>
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}">Register</a>
+                                @endif
+                            @endauth
+                    @endif
                     <a href="{{ route('article.index') }}">View Articles</a>
-                    <a href="{{ route('article.create') }}">Create</a>
                     <a href="https://github.com/fey/laravel-blog">GitHub</a>
                 </div>
             </div>
