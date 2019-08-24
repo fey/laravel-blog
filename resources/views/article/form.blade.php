5291bd76d7557f95c21decefda32ceddeed483fc
feycot <feycot@gmail.com>
2019-08-24 11:56:42 +0500

    fix form

--- resources/views/article/form.blade.php
+++ resources/views/article/form.blade.php
@@ -1,30 +1,32 @@
-      <div class="form-group">
-        @error('name')
-          <div class="alert alert-danger" role="alert">{{ $message }}</div>
-        @enderror
-        {{ Form::label('name', 'Название') }}
-        {{ Form::text('name', null, ['class' => 'form-control']) }}
-      </div>
-      <div class="form-group">
-        @error('category')
-          <div class="alert alert-danger" role="alert">{{ $message }}</div>
-        @enderror
-        {{ form::label('category_id', 'Category') }}
-        {{ Form::select('category_id', $categories, null, ['class'=>'form-control',]) }}
-      </div>
-        @error('tags')
-          <div class="alert alert-danger" role="alert">{{ $message }}</div>
-        @enderror
-        @foreach ($tags->all() as $tag)
-      <div class="form-check form-check-inline">
-          {{ Form::checkbox('tags[]', $tag->id, null, ['class'=>'form-check-input']) }}
-          {{ Form::label('tags', $tag->name, ['class' => 'form-check-label)']) }}
-      </div>
-        @endforeach
-      <div class="form-group">
-        @error('body')
-          <div class="alert alert-danger" role="alert">{{ $message }}</div>
-        @enderror
-        {{ Form::label('body', 'Текст') }}
-        {{ Form::textarea('body', null, ['class'=>'form-control',]) }}
-      </div>
+<div class="form-group">
+    @error('name')
+    <div class="alert alert-danger" role="alert">{{ $message }}</div>
+    @enderror
+    {{ Form::label('name', 'Название') }}
+    {{ Form::text('name', null, ['class' => 'form-control']) }}
+</div>
+@if ($categories->isNotEmpty())
+<div class="form-group">
+    @error('category')
+    <div class="alert alert-danger" role="alert">{{ $message }}</div>
+    @enderror
+    {{ Form::label('category_id', 'Category') }}
+    {{ Form::select('category_id', $categories, null, ['class'=>'form-control',]) }}
+</div>
+@endif
+@error('tags')
+<div class="alert alert-danger" role="alert">{{ $message }}</div>
+@enderror
+@foreach ($tags->all() as $tag)
+<div class="form-check form-check-inline">
+    {{ Form::checkbox('tags[]', $tag->id, null, ['class'=>'form-check-input']) }}
+    {{ Form::label('tags', $tag->name, ['class' => 'form-check-label)']) }}
+</div>
+@endforeach
+<div class="form-group">
+    @error('body')
+    <div class="alert alert-danger" role="alert">{{ $message }}</div>
+    @enderror
+    {{ Form::label('body', 'Текст') }}
+    {{ Form::textarea('body', null, ['class'=>'form-control',]) }}
+</div>
